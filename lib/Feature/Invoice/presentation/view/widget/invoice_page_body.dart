7e965a6adba50200a57542857446422e816ae064
Mina Kamil <mena.m4077@gmail.com>
2026-03-08 01:47:46 +0200

    Add class Asset paths

--- lib/Feature/Invoice/presentation/view/widget/invoice_page_body.dart
+++ lib/Feature/Invoice/presentation/view/widget/invoice_page_body.dart
@@ -3,6 +3,8 @@ import 'package:get/state_manager.dart';
 import 'package:invoicepro/Feature/Invoice/presentation/view/controller/invoice_controller.dart';
 import 'package:invoicepro/Feature/Invoice/presentation/view/widget/InvoiceDesign.dart';
 import 'package:invoicepro/Feature/Invoice/presentation/view/widget/invoice_listView_prodact_item.dart';
+import 'package:invoicepro/utils/Asset_Paths.dart';
+import 'package:invoicepro/utils/widgets/app_textView.dart';
 
 class InvoicePageBody extends GetView<InvoiceController> {
   const InvoicePageBody({super.key});
@@ -18,11 +20,18 @@ class InvoicePageBody extends GetView<InvoiceController> {
             padding: const EdgeInsets.all(12),
             child: Column(
               children: [
-                Card(
-                  child: ElevatedButton(
-                    onPressed: controller.addItem,
-                    child: const Text("إضافة منتج"),
+                ElevatedButton(
+                  child: Text('Elevated Button'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
                   ),
+                  onPressed: () {},
+                ),
+                ElevatedButton(
+                  onPressed: controller.addItem,
+                  style: ButtonStyle(),
+                  child: AppText(text: "زوايا امامى"),
                 ),
                 const SizedBox(height: 10),
                 InvoiceListViewProdactItem(controller: controller),
---
+++ lib/utils/Asset_Paths.dart
@@ -0,0 +1,5 @@
+class AssetPaths {
+  static const String logo = 'assets/image/logo.jpg';
+
+  static const String qrCode = 'assets/image/qr_code.jpeg';
+}
